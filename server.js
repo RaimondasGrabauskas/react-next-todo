1e1f01378409494f44586da6f50ad0d05d49a666
Raimis <rggrabauskas@gmail.com>
2021-09-27 10:27:29 +0300

    server running

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = 4000;
+
+mongoose
+  .connect(process.env.MONGO_CONNECT_STIRNG, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Conneced to Mongoosee');
+  })
+  .catch((err) => console.error(err.message));
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, console.log(`server running on port ${PORT}`));
