edcf8c6edac692122ff8352bf31fffc1cb535583
Raimis <rggrabauskas@gmail.com>
2021-10-12 12:59:19 +0300

    initials todos, and get all todos from database

---
+++ client/src/store/todoRedux.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const getAllTodoUrl = 'http://localhost:4000/todos';
+const addTodoUrl = 'http://localhost:4000/todo/new';
+
+export const getTodos = createAsyncThunk('todos/getTodos', async () => {
+  const response = await axios.get(getAllTodoUrl);
+  return response.data;
+});
+
+export const addTodo = createAsyncThunk('todos/addTodo');
+
+const initialState = { todos: [], status: null };
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo(state, action) {
+      state.todos.push(action.payload);
+    },
+  },
+  extraReducers: {
+    [getTodos.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [getTodos.fulfilled]: (state, action) => {
+      state.todos = action.payload;
+      state.status = 'success';
+    },
+    [getTodos.rejected]: (state, action) => {
+      state.status = 'failed';
+    },
+  },
+});
+
+export const todoActions = todoSlice.actions;
+export default todoSlice.reducer;
