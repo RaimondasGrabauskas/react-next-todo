41b8ed05324e98a1b2ee465eb3e94ed01c2c4fe1
Raimis <rggrabauskas@gmail.com>
2021-11-09 18:56:35 +0200

    add delete todo route, edit todo route

--- server/Routes/TodoRoutes.js
+++ server/Routes/TodoRoutes.js
@@ -2,20 +2,6 @@ const express = require('express');
 const TodoData = require('../modules/todoModule');
 const router = express.Router();
 
-// create new todo route
-
-router.post('/todo/new', async (req, res) => {
-  // const newTodoDetails = req.body;
-  const newTodo = new TodoData(req.body);
-
-  try {
-    const creatingNewTodoResult = await newTodo.save();
-    res.json(creatingNewTodoResult);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // get all todos route
 
 router.get('/todos', async (req, res) => {
@@ -27,4 +13,41 @@ router.get('/todos', async (req, res) => {
   }
 });
 
+// create new todo route
+
+router.post('/todo/new', async (req, res) => {
+  const newTodoDeteil = req.body;
+  const newTodo = new TodoData(newTodoDeteil);
+  try {
+    const creatingNewTodoResult = await newTodo.save();
+    res.json(creatingNewTodoResult);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete todo
+router.delete('/todo/delete/:id', async (req, res) => {
+  const todoDeleteId = req.params.id;
+  try {
+    const deleteResult = await TodoData.findByIdAndDelete(todoDeleteId);
+    res.json(deleteResult);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// update todo
+
+router.put('/todo/update/:id', async (req, res) => {
+  const updateTodoId = req.params.id;
+  const detailsToUpdate = req.body;
+
+  try {
+    const updateResult = await TodoData.findByIdAndUpdate(updateTodoId, detailsToUpdate);
+    res.json(updateResult);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
