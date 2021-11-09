a54283b3be9108b246241e1c82a7e99773e08906
Raimis <rggrabauskas@gmail.com>
2021-11-09 18:58:10 +0200

    add todo works fine

--- client/src/components/addTodo/AddTodo.jsx
+++ client/src/components/addTodo/AddTodo.jsx
@@ -1,12 +1,40 @@
 import css from './AddTodo.module.css';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createTodo } from '../../utils/request';
+import { getTodos } from './../../store/todoRedux';
 
 const AddTodo = () => {
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState('');
+  
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const clearInput = () => {
+    setInputValue('');
+  }
+
+  const addTodo = async () => {
+    const newTodoDetail = {
+      title: inputValue,
+    };
+    const successCreated = await createTodo(newTodoDetail);
+    if (successCreated) {
+      dispatch(getTodos());
+    }
+    clearInput();
+  }
+
   return (
     <div className={css['add-todo-container']}>
-        <i  className="fa fa-plus-circle add-icon"></i>
+        <i  className="fa fa-plus-circle add-icon" onClick={addTodo} ></i>
         <input
           type="text"
           placeholder="Add new Todo"
+          value={inputValue}
+          onChange={handleInput}
         />
       </div>
   )
