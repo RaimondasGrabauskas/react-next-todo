86ba7782ff27f900b345a5e3632d1fe69d5701c9
Raimis <rggrabauskas@gmail.com>
2021-11-09 18:58:37 +0200

    edit and delete todo works fine

--- client/src/components/todoElement/TodoElement.jsx
+++ client/src/components/todoElement/TodoElement.jsx
@@ -1,12 +1,57 @@
+import { useDispatch } from 'react-redux';
 import css from './TodoElement.module.css';
+import { getTodos } from './../../store/todoRedux';
+import { deleteTodo, updateTodo } from '../../utils/request';
+import { useState, useEffect } from 'react';
 
 const TodoElement = ({todo}) => {
+  const dispatch = useDispatch();
+  const [isEditOn, setIsEditOn] = useState(false);
+  const [newTitle, setNewTitle] = useState('');
+
+
+  const handleIsEditOn = () => {
+    setIsEditOn(!isEditOn);
+  }
+
+  const handleChangeInputValue = (e) => {
+  
+    // const newInputValueCopy = (' ' + newTitle).slice(1);
+    // newInputValueCopy[e.target.name] = e.target.value;
+    setNewTitle(e.target.value);
+  }
+
+  const deleteTodoItem = async (id) => {
+    const deletedSuccess = await deleteTodo(id);
+    if (deletedSuccess) {
+      dispatch(getTodos());
+    }
+  }
+
+  const editTodo = async (id) => {
+    const editDetails = {
+      title: newTitle,
+    };
+    const updateSuccess = await updateTodo(id, editDetails);
+    if (updateSuccess) {
+      dispatch(getTodos());
+    }
+    handleIsEditOn();
+  }
+
+  useEffect(() => {
+    setNewTitle(todo.title);
+  },[])
+
+
   
   return (
     <li className={css['app-todo-el']}>
-      {todo.title}
-      <i className="fa fa-pencil"></i>
-      <i className="fa fa-trash"></i>
+      {isEditOn && <input value={newTitle} onChange={handleChangeInputValue} name='newTitle'/>}
+      {!isEditOn && todo.title}
+      {!isEditOn && <i className="fa fa-pencil" onClick={handleIsEditOn}></i>}
+      {isEditOn && <i className="fa fa-pencil" onClick={() => editTodo(todo._id)}></i>}
+      <i onClick={() => deleteTodoItem(todo._id)} className="fa fa-trash"></i>
     </li>
   )
 }
